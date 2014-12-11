8db84d0f0bdd6c86f5b0f0cfdc569d8ba143e9d5
zulan <github@zulan.net>
2014-12-11 22:55:48 +0100

    Fixed global/local settings

--- civdj/civdj/settings.py
+++ civdj/civdj/settings.py
@@ -12,32 +12,20 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xmmqy@84(*8etpafq!zupl0psr%mg11z1nr8pe-hb77f==ukx2'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
+DEBUG = False
+TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = []
 
-
 # Application definition
-
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'registration',
     'polymorphic',
     'pbspy',
+    'registration',
     'django.contrib.admin',
     'debug_toolbar',
     'erroneous',
@@ -60,20 +48,8 @@ ROOT_URLCONF = 'civdj.urls'
 
 WSGI_APPLICATION = 'civdj.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/dev/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -84,28 +60,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/dev/howto/static-files/
-
-if DEBUG == True:
-  STATIC_ROOT='static/'
-  STATIC_URL = '/static/'
-else:
-  # Call 'python manage.py collectstatic' to transfer
-  # files into STATIC_ROOT
-  STATIC_ROOT='/var/www/pbspy/static/'
-  STATIC_URL = 'http://localhost/pbspy/static/'
-
 DEBUG_TOOLBAR_CONFIG = {
     'SHOW_TEMPLATE_CONTEXT': True,
 }
 
 ACCOUNT_ACTIVATION_DAYS = 7
-
-EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = '/tmp/django-messages'
-DEFAULT_FROM_EMAIL = 'pbspy@zulan.net'
 
 LOGIN_REDIRECT_URL = 'game_list'
 
@@ -116,4 +75,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     # other finders..
     'static_precompiler.finders.StaticPrecompilerFinder',
-    )
+)
+
+
+from civdj.settings_local import *
