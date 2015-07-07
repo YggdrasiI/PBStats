c2a3b1d99fc35395be4e65d064caf0a4618b2cd6
Zulan <github@zulan.net>
2015-07-07 19:49:01 +0200

    - Removed redundant Versioning

--- tests/fix_upload_bug/Linux/pitbossUploadFix.py
+++ tests/fix_upload_bug/Linux/pitbossUploadFix.py
@@ -3,25 +3,25 @@
 # This script analyse your network traffic to
 # detect the 'upload bug' which cause a massive paket spamming
 # from a Civ4 Pitboss server. After a detection it fakes a udp paket
-# with the content 'player X quit connection'. 
+# with the content 'player X quit connection'.
 #
-# Requirements: 
-# -	sudo apt-get install libnet1-dev libpcap0.8-dev 
+# Requirements:
+# -	sudo apt-get install libnet1-dev libpcap0.8-dev
 # - Python wrapper http://sourceforge.net/projects/pyip/
-# - Python wrapper https://github.com/Onuonga/pycap 
+# - Python wrapper https://github.com/Onuonga/pycap
 #
 # Notes:
 # - Script requires 'sudo' to get access to the network traffic.
 # - If you get the following error message:
 #     '[...]ImportError: No module named sll'
-#   , then remove 'ssl' from list in pycap/constants/__init__.py. 
+#   , then remove 'ssl' from list in pycap/constants/__init__.py.
 #
 #
 
 import time, socket, sys, os
 
 # Add subfolders to python paths for imports
-# Remove this lines if you has install the packages 
+# Remove this lines if you has install the packages
 sys.path.append(os.path.join(os.path.dirname(__file__), 'pyip-0.7'))
 sys.path.append(os.path.join(os.path.dirname(__file__), 'pycap-2.0'))
 # Add pycap and pyip
@@ -33,19 +33,29 @@ import ip as ip2
 import udp as udp2
 
 
-# === Configuration === 
+# === Configuration ===
 
 device = "eth0" # Interface name
-server_ip = "192.168.0.1" # Ip of your PB Server
-server_portLow = 2056 # Default value if you use no arguments
-server_portHigh = server_portLow 
+#server_ip = "148.251.126.92" # Ip of your PB Server
+server_ip = "192.168.0.35" # Ip of your PB Server
 
+## server_portlist contains list (seperated by ,)
+# of single ports (P) or portranges (P1-P2).
+#
+# format: (P|P1-P2)[,Q|Q1-Q2[,...]]
+#
+server_portlist = "2056" # Default value if you use no arguments.
+logfileName = "detections.log" # Default logfile name
+
+# Note: If the client
+package_limit = 200
+package_counter = {}
 timeout = 500
 clients = {}
 
 
 
-# === Send Fake Paket === 
+# === Send Fake Paket ===
 # Inject paket, but fake client ip
 # src,dst : (ip,port) tuple
 def sendUdpReply(src,dst,data):
@@ -73,24 +83,39 @@ def sendUdpReply(src,dst,data):
 	#print ipacket
 
 
-# === Analyse Traffic === 
+# === Analyse Traffic ===
 # device: "eth0" or other devicename (string)
 # server: (ip,port) tuple or (ip,port1,port2) triple for port range
 # clients: List
 # timeout: Waiting time on package
 def analyseUdpTraffic(device, server, clients, timeout):
 
-	# Note: I disable filtering by host.
-	if( len(server)>2 ):
-		filter = "(udp portrange {port1}-{port2} )".format( host = server[0], port1 = server[1], port2 = server[1])
-	else:
-		filter = "(udp port {port} )".format( host = server[0], port = server[1])
+	# Construct filter string
+	port_str = "udp and ( "
+	portlist_first = True
+	portlist = str(server[1]).split(',')
+	for p in portlist:
+		portrange = p.split('-')
+		if not portlist_first:
+			port_str += " or "
+		else:
+			portlist_first = False
+
+		if len(portrange) > 1:
+			port_str += "portrange {}-{}".format( int(portrange[0]), int(portrange[1]) )
+		else:
+			port_str += "port {}".format( int(portrange[0]) )
+
+	port_str += " )"
+
+	# Note: Filter does not respect PB host ip"
+	filter = port_str
+	#print "Used filter: ", filter
 
 	pcap = pycap.capture.capture(device, timeout = timeout)
 	pcap.filter(filter);
 
 	nCaptureErrors = 0
-	nHeartbeatCounter = 0
 
 	while True:
 		try:
@@ -116,91 +141,90 @@ def analyseUdpTraffic(device, server, clients, timeout):
 		if (ip.source == server[0]):
 			client = ip.destination
 
-			# Anlayse content if payload is at least 20 chars long
-			pl = len(payload)
-			if pl == 25 and ord(payload[0]) == 254 and ord(payload[1]) == 254:
-				# Structure of searched payload:
-				# 254 254 00
-				# [2 bytes number: A]
-				# [2 bytes number: B]
-				# [11 bytes: content]
-				# [7 bytes: not analysed]
+			if len(payload) != 25 and len(payload) != 30:
+				continue
 
-				clist = clients.get(client,[])
-				clist.append( payload )
-				clients[client] = clist 
+			# This package could be indicate an upload error. Add the payload
+			# for this client (destination ip) to an set. Force analysation
+			# of the packages if an sufficient amount of packages reached.
+			#
+			# The length 37 occours if the connections was aborted during the loading
+			# of a game.
+			#
 
-				N = len(clist)
-				if N > 10:
-					#Check if B + content is always the same -> upload bug detected
-					bugFound = True
-					for n in xrange(N-10,N):
-						if clist[N-1][5:18] != clist[n][5:18] :
-							bugFound = False
-							break
+			pc = package_counter.get(client, 0)
+			pc += 1
+			package_counter[client] = pc
 
-					# Flush list
-					clients[client] = []
+			if pc > package_limit:
+				#print packet
+				# Send fake packet to stop upload
+				# Structure of content:
+				# 254 254 06 B (A+1) (7 bytes)
+				A = payload[3:5] # String!
+				B = payload[5:7]
+				a1 = ord(A[0])*256 + ord(A[1])+1
+				A1 = chr(a1/256) + chr(a1%256)
+				#print (a1,A1,A)
+				src = (packet[1].destination,packet[2].destinationport)
+				dst = (packet[1].source,packet[2].sourceport)
+				data = chr(254)+chr(254)+chr(06) + B + A1
 
-					#print ("Upload-Bug found?! ", bugFound )
-					if bugFound:
-						#print packet
-						# Send fake packet to stop upload
-						# Structure of content: 
-						# 254 254 06 B (A+1) (7 bytes)
-						A = clist[N-1][3:5] # String!
-						B = clist[N-1][5:7]
-						a1 = ord(A[0])*256 + ord(A[1])+1
-						A1 = chr(a1/256) + chr(a1%256)
-						#print (a1,A1,A)
+				msg = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
+				msg += " | %s:%s | %s:%s \n" % (src[0], src[1], dst[0], dst[1])
 
-						src = (packet[1].destination,packet[2].destinationport)
-						dst = (packet[1].source,packet[2].sourceport)
-						data = chr(254)+chr(254)+chr(06) + B + A1
+				print msg,
+				if logfile != None:
+					logfile.write(msg)
+					logfile.flush()
 
-						print "Upload bug detected send fake packet for client %s:%s to server %s:%s" % (src[0], src[1], dst[0], dst[1])
-						#print ord(data[0]),ord(data[1]),ord(data[2]),ord(data[3]),ord(data[4]),ord(data[5]),ord(data[6])
-						print "%X %X %X %X %X %X %X" % (ord(data[0]),ord(data[1]),ord(data[2]),ord(data[3]),ord(data[4]),ord(data[5]),ord(data[6]))
-						sendUdpReply(src,dst,data)
+				#comment in do activate kicking....
+				sendUdpReply(src,dst,data)
 
-						# End detection for all clients
-						break
+				# End detection for all clients
+				break
 
-		if (ip.destination == server[0]):
+		elif (ip.destination == server[0]):
 			#Use this package as client heartbeat and flush the history of tracked packages.
 			client = ip.source
-			clients[client] = []
-			print "Heartbeat!"
-			nHeartbeatCounter += 1
-			if nHeartbeatCounter == 1000:
-				#Many activity on the server. Sleep some time and try detection later.
-				print "Heartbeat Sleep!"
-				time.sleep(300)
-				return;
+			if package_counter.get(client) > 100:
+				print "Heartbeat after ", package_counter.get(client), " server packages!"
+			package_counter[client] = 0
+		else:
+			print "IP confusion. PB server neither match source or destination."
+			print "Serverip:", server[0]
+			print "Source:", ip.destination
+			print "Dest:", ip.destination
 
-		if( len(clients) > 200 ):
-			# short list of stored clients
-			clients = {}
-
-
-
-# === Main === 
+# === Main ===
 if len(sys.argv) < 2:
-	print "Usage: ./", sys.argv[0] , "[port]", "[port]", "[network device]" 
-	print "No arguments given. Assume default interface %s and Pitboss server portrange=%i-%i." % (device, server_portLow, server_portHigh)
+	print "Usage: ./", sys.argv[0] , "[portlist]", "[network device]", "[logfile]"
+	print "No arguments given. Assume default interface %s,\n Pitboss server portlist=%s, and \n Logfile %s." % (device, server_portlist, logfileName)
 else:
-	server_portLow = int(sys.argv[1])
+	server_portlist = sys.argv[1]
 
 	if len(sys.argv) > 2:
-		server_portHigh = int(sys.argv[2])
-	else:
-		server_portHigh = server_portLow
+		device = sys.argv[2]
 
 	if len(sys.argv) > 3:
-		device = sys.argv[3]
-	
-	print "Use network device %s and Pitboss server portrange=%i-%i." % (device, server_portLow, server_portHigh)
+		logfileName = sys.argv[3]
+
+	if len(logfileName) > 0:
+		logfile = file(logfileName,"a")
+	else:
+		logfile = None
+
+	print "Use network device %s and Pitboss server portlist=%s." % (device, server_portlist)
+	print "\n\n"
+	print " List of detected upload bugs"
+
+	msg = "      Timestamp           |    Client         |    Server          \n"
+	print msg,
+	if logfile != None:
+		logfile.write(msg)
+		logfile.flush()
+
 
 while True:
-	analyseUdpTraffic(device, (server_ip,server_portLow,server_portHigh), clients, timeout)
+	analyseUdpTraffic(device, (server_ip,server_portlist), clients, timeout)
 	time.sleep(60)
