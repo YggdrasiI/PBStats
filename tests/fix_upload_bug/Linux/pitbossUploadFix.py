277feb8997ae912ea15d748d7ceacc259b6eec61
Zulan <github@zulan.net>
2015-07-07 19:52:16 +0200

    - Fixed tabs -> 4 spaces cause pep8!

--- tests/fix_upload_bug/Linux/pitbossUploadFix.py
+++ tests/fix_upload_bug/Linux/pitbossUploadFix.py
@@ -6,7 +6,7 @@
 # with the content 'player X quit connection'.
 #
 # Requirements:
-# -	sudo apt-get install libnet1-dev libpcap0.8-dev
+# - sudo apt-get install libnet1-dev libpcap0.8-dev
 # - Python wrapper http://sourceforge.net/projects/pyip/
 # - Python wrapper https://github.com/Onuonga/pycap
 #
@@ -59,28 +59,28 @@ clients = {}
 # Inject paket, but fake client ip
 # src,dst : (ip,port) tuple
 def sendUdpReply(src,dst,data):
-	upacket = udp2.Packet()
-	upacket.sport = src[1]
-	upacket.dport = dst[1]
-	upacket.data = data
+    upacket = udp2.Packet()
+    upacket.sport = src[1]
+    upacket.dport = dst[1]
+    upacket.data = data
 
-	ipacket = ip2.Packet()
-	ipacket.src = src[0]
-	ipacket.dst = dst[0]
-	ipacket.df = 1
-	ipacket.ttl = 64
-	ipacket.p = 17
+    ipacket = ip2.Packet()
+    ipacket.src = src[0]
+    ipacket.dst = dst[0]
+    ipacket.df = 1
+    ipacket.ttl = 64
+    ipacket.p = 17
 
-	ipacket.data = udp2.assemble(upacket, False)
-	raw_ip = ip2.assemble(ipacket, 1)
+    ipacket.data = udp2.assemble(upacket, False)
+    raw_ip = ip2.assemble(ipacket, 1)
 
-	try:
-		sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
-	except socket.error , msg:
-		print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
+    except socket.error , msg:
+        print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
 
-	sock.sendto(raw_ip, (ipacket.dst, 0))
-	#print ipacket
+    sock.sendto(raw_ip, (ipacket.dst, 0))
+    #print ipacket
 
 
 # === Analyse Traffic ===
@@ -90,141 +90,141 @@ def sendUdpReply(src,dst,data):
 # timeout: Waiting time on package
 def analyseUdpTraffic(device, server, clients, timeout):
 
-	# Construct filter string
-	port_str = "udp and ( "
-	portlist_first = True
-	portlist = str(server[1]).split(',')
-	for p in portlist:
-		portrange = p.split('-')
-		if not portlist_first:
-			port_str += " or "
-		else:
-			portlist_first = False
+    # Construct filter string
+    port_str = "udp and ( "
+    portlist_first = True
+    portlist = str(server[1]).split(',')
+    for p in portlist:
+        portrange = p.split('-')
+        if not portlist_first:
+            port_str += " or "
+        else:
+            portlist_first = False
 
-		if len(portrange) > 1:
-			port_str += "portrange {}-{}".format( int(portrange[0]), int(portrange[1]) )
-		else:
-			port_str += "port {}".format( int(portrange[0]) )
+        if len(portrange) > 1:
+            port_str += "portrange {}-{}".format( int(portrange[0]), int(portrange[1]) )
+        else:
+            port_str += "port {}".format( int(portrange[0]) )
 
-	port_str += " )"
+    port_str += " )"
 
-	# Note: Filter does not respect PB host ip"
-	filter = port_str
-	#print "Used filter: ", filter
+    # Note: Filter does not respect PB host ip"
+    filter = port_str
+    #print "Used filter: ", filter
 
-	pcap = pycap.capture.capture(device, timeout = timeout)
-	pcap.filter(filter);
+    pcap = pycap.capture.capture(device, timeout = timeout)
+    pcap.filter(filter);
 
-	nCaptureErrors = 0
+    nCaptureErrors = 0
 
-	while True:
-		try:
-			if nCaptureErrors > 20 :
-				# Sleep a minute
-				break
-			packet = pcap.next()
-		except pycap.capture.error:
-			nCaptureErrors += 1
-			continue
-		if packet is None:
-			nCaptureErrors += 1
-			continue
+    while True:
+        try:
+            if nCaptureErrors > 20 :
+                # Sleep a minute
+                break
+            packet = pcap.next()
+        except pycap.capture.error:
+            nCaptureErrors += 1
+            continue
+        if packet is None:
+            nCaptureErrors += 1
+            continue
 
-		nCaptureErrors = 0
+        nCaptureErrors = 0
 
-		ip = packet[1]
-		udp = packet[2]
-		payload = packet[3]
-		assert isinstance(ip, pycap.protocol.ip)
-		assert isinstance(udp, pycap.protocol.udp)
+        ip = packet[1]
+        udp = packet[2]
+        payload = packet[3]
+        assert isinstance(ip, pycap.protocol.ip)
+        assert isinstance(udp, pycap.protocol.udp)
 
-		if (ip.source == server[0]):
-			client = ip.destination
+        if (ip.source == server[0]):
+            client = ip.destination
 
-			if len(payload) != 25 and len(payload) != 30:
-				continue
+            if len(payload) != 25 and len(payload) != 30:
+                continue
 
-			# This package could be indicate an upload error. Add the payload
-			# for this client (destination ip) to an set. Force analysation
-			# of the packages if an sufficient amount of packages reached.
-			#
-			# The length 37 occours if the connections was aborted during the loading
-			# of a game.
-			#
+            # This package could be indicate an upload error. Add the payload
+            # for this client (destination ip) to an set. Force analysation
+            # of the packages if an sufficient amount of packages reached.
+            #
+            # The length 37 occours if the connections was aborted during the loading
+            # of a game.
+            #
 
-			pc = package_counter.get(client, 0)
-			pc += 1
-			package_counter[client] = pc
+            pc = package_counter.get(client, 0)
+            pc += 1
+            package_counter[client] = pc
 
-			if pc > package_limit:
-				#print packet
-				# Send fake packet to stop upload
-				# Structure of content:
-				# 254 254 06 B (A+1) (7 bytes)
-				A = payload[3:5] # String!
-				B = payload[5:7]
-				a1 = ord(A[0])*256 + ord(A[1])+1
-				A1 = chr(a1/256) + chr(a1%256)
-				#print (a1,A1,A)
-				src = (packet[1].destination,packet[2].destinationport)
-				dst = (packet[1].source,packet[2].sourceport)
-				data = chr(254)+chr(254)+chr(06) + B + A1
+            if pc > package_limit:
+                #print packet
+                # Send fake packet to stop upload
+                # Structure of content:
+                # 254 254 06 B (A+1) (7 bytes)
+                A = payload[3:5] # String!
+                B = payload[5:7]
+                a1 = ord(A[0])*256 + ord(A[1])+1
+                A1 = chr(a1/256) + chr(a1%256)
+                #print (a1,A1,A)
+                src = (packet[1].destination,packet[2].destinationport)
+                dst = (packet[1].source,packet[2].sourceport)
+                data = chr(254)+chr(254)+chr(06) + B + A1
 
-				msg = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
-				msg += " | %s:%s | %s:%s \n" % (src[0], src[1], dst[0], dst[1])
+                msg = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
+                msg += " | %s:%s | %s:%s \n" % (src[0], src[1], dst[0], dst[1])
 
-				print msg,
-				if logfile != None:
-					logfile.write(msg)
-					logfile.flush()
+                print msg,
+                if logfile != None:
+                    logfile.write(msg)
+                    logfile.flush()
 
-				#comment in do activate kicking....
-				sendUdpReply(src,dst,data)
+                #comment in do activate kicking....
+                sendUdpReply(src,dst,data)
 
-				# End detection for all clients
-				break
+                # End detection for all clients
+                break
 
-		elif (ip.destination == server[0]):
-			#Use this package as client heartbeat and flush the history of tracked packages.
-			client = ip.source
-			if package_counter.get(client) > 100:
-				print "Heartbeat after ", package_counter.get(client), " server packages!"
-			package_counter[client] = 0
-		else:
-			print "IP confusion. PB server neither match source or destination."
-			print "Serverip:", server[0]
-			print "Source:", ip.destination
-			print "Dest:", ip.destination
+        elif (ip.destination == server[0]):
+            #Use this package as client heartbeat and flush the history of tracked packages.
+            client = ip.source
+            if package_counter.get(client) > 100:
+                print "Heartbeat after ", package_counter.get(client), " server packages!"
+            package_counter[client] = 0
+        else:
+            print "IP confusion. PB server neither match source or destination."
+            print "Serverip:", server[0]
+            print "Source:", ip.destination
+            print "Dest:", ip.destination
 
 # === Main ===
 if len(sys.argv) < 2:
-	print "Usage: ./", sys.argv[0] , "[portlist]", "[network device]", "[logfile]"
-	print "No arguments given. Assume default interface %s,\n Pitboss server portlist=%s, and \n Logfile %s." % (device, server_portlist, logfileName)
+    print "Usage: ./", sys.argv[0] , "[portlist]", "[network device]", "[logfile]"
+    print "No arguments given. Assume default interface %s,\n Pitboss server portlist=%s, and \n Logfile %s." % (device, server_portlist, logfileName)
 else:
-	server_portlist = sys.argv[1]
+    server_portlist = sys.argv[1]
 
-	if len(sys.argv) > 2:
-		device = sys.argv[2]
+    if len(sys.argv) > 2:
+        device = sys.argv[2]
 
-	if len(sys.argv) > 3:
-		logfileName = sys.argv[3]
+    if len(sys.argv) > 3:
+        logfileName = sys.argv[3]
 
-	if len(logfileName) > 0:
-		logfile = file(logfileName,"a")
-	else:
-		logfile = None
+    if len(logfileName) > 0:
+        logfile = file(logfileName,"a")
+    else:
+        logfile = None
 
-	print "Use network device %s and Pitboss server portlist=%s." % (device, server_portlist)
-	print "\n\n"
-	print " List of detected upload bugs"
+    print "Use network device %s and Pitboss server portlist=%s." % (device, server_portlist)
+    print "\n\n"
+    print " List of detected upload bugs"
 
-	msg = "      Timestamp           |    Client         |    Server          \n"
-	print msg,
-	if logfile != None:
-		logfile.write(msg)
-		logfile.flush()
+    msg = "      Timestamp           |    Client         |    Server          \n"
+    print msg,
+    if logfile != None:
+        logfile.write(msg)
+        logfile.flush()
 
 
 while True:
-	analyseUdpTraffic(device, (server_ip,server_portlist), clients, timeout)
-	time.sleep(60)
+    analyseUdpTraffic(device, (server_ip,server_portlist), clients, timeout)
+    time.sleep(60)
