ac6ec6dbf6f9e78fae11f6640df7b4aeb2e00a52
Olaf Schulz <funwithkinect@googlemail.com>
2015-07-24 02:13:39 +0200

    Add propagation of xvfb-run session cookies to startup script.

--- PBs/startPitboss.sh
+++ PBs/startPitboss.sh
@@ -261,8 +261,23 @@ setupGame "${ALTROOT}" "$2" "$3"
 cd "$CIV4BTS_PATH"
 for(( ; ; )) do
 	if [ -z "$DISPLAY" ]; then
-		echo "No display detected. Starting with xvfb-run."
-		xvfb-run -s "-screen 0 640x480x24" wine "$CIV4BTS_EXE"  mod= "${MOD}"\" /ALTROOT="${ALTROOT_W}"
+		echo "No display detected, running with xvfb-run"
+
+		# Use this file for X11 authentication of watchdog
+		MCOOKIE=/tmp/${ALTROOT##*/}
+		rm ${MCOOKIE}
+
+		# Enable job control for non-interactive shell
+		set -m
+
+		# Invoke Xvfb
+		xvfb-run -a  -e /dev/shm/xvfb.err --auth-file=${MCOOKIE} -s " -fbdir /var/tmp -screen 0 640x480x24" wine "$CIV4BTS_EXE"  mod= "${MOD}"\" /ALTROOT="${ALTROOT_W}" &
+
+		# Propagate cookie and wait
+		sleep 2
+		xauth merge ${MCOOKIE}
+		fg
+
 	else
 		wine "$CIV4BTS_EXE"  mod= "${MOD}"\" /ALTROOT="${ALTROOT_W}"
 	fi
--- tests/watchdog/killPitbossInstance.sh
+++ tests/watchdog/killPitbossInstance.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 # 
 # 
-#
+
+# pbSettings["save"]["filename"] =
+#   pbSettings["save"]["previous_filename"]
+load_previous_save () {
+	ALTROOT="$1"
+	grep "['\"]previous_filename['\"][ \t]*:" >/dev/null 2>&1 <"$ALTROOT/pbSettings.json" 
+	if [ 0 -eq "$?" ] ; 
+	then
+		#echo "Replace filename."
+		sed -e "/['\"]\(filename\|folderIndex\)['\"][ \t]*:/d" \
+			-e "s/['\"]previous_filename['\"][ \t]*:/\"filename\":/" \
+			-e "s/['\"]previous_folderIndex['\"][ \t]*:/\"folderIndex\":/" \
+			-i "$ALTROOT/pbSettings.json"
+	#else
+		#echo "No previous filename found."
+	fi
+}
 
 if [ "$1" = "-h" ] ; 
 then
@@ -10,7 +26,7 @@ then
 	Use the name of the	ALTROOT directory as identifier.
 	The full ALTROOT path shouldn't contain spaces.
 
-	usage: ./$0 [Altroot directory, default: PB1]"
+	usage: ./$0 [Altroot directory, default: PB1] [Use previous save, optional flag ]"
 else
 	PB=${1:-PB1}
 	FILTER="ALTROOT=[^ ]*$PB"
@@ -24,6 +40,13 @@ else
 	then
 		#echo "PID: $PID, Line: $LINE"
 		kill -9 $PID
+		if [ "$2" = "1" ] ;
+		then
+			ALTROOT=$(echo ${LINE##*ALTROOT=Z:})
+			ALTROOT=${ALTROOT//\\/\/}
+			#echo "Linux altroot: ${ALTROOT}"
+			load_previous_save "$ALTROOT"
+		fi
 		#return 0
 	else
 		echo "Can't find unique PID for '${1}'. Abort process kill."
