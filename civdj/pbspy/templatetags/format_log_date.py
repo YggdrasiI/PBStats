eb4be9e39a19bbd6d6fd01374695e8872d394ab1
Olaf Schulz <funwithkinect@googlemail.com>
2015-07-28 22:56:09 +0200

    PBSpy: Remove display of time for old log entries (inefficent evaluation).

---
+++ civdj/pbspy/templatetags/format_log_date.py
@@ -0,0 +1,23 @@
+
+from django import template
+from datetime import datetime, timedelta
+from django.utils import timezone
+
+from django.utils import formats
+from django.conf import settings
+
+register = template.Library()
+
+
+def log_date(value, arg):  # , date_now, distance_in_s):
+        """Coarse displayed timestamp of old entries."""
+        #now = datetime.now()  # wrong timezone offset
+        now = timezone.now()
+        delta = now - value
+        if(delta.days >= arg):
+            return formats.date_format(value, "SHORT_DATE_FORMAT")
+        else:
+            return formats.date_format(value, "SHORT_DATETIME_FORMAT")
+
+
+register.filter('log_date', log_date)
