45d257cf729f259957b016f0ccc5fa35bbfe6f8f
Zulan <github@zulan.net>
2015-07-31 20:00:10 +0200

    - added privacy enhanced player entry - refactored log entry a bit

---
+++ civdj/pbspy/templatetags/format_log.py
@@ -0,0 +1,36 @@
+
+from django import template
+from datetime import datetime, timedelta
+from django.utils import timezone
+
+from django.utils import formats
+from django.conf import settings
+
+register = template.Library()
+
+
+def log_date_privacy(log_date, threshold_days):
+    """Removes time from datetimes older than threshold_days days."""
+    now = timezone.now()
+    delta = now - log_date
+    if delta.days >= threshold_days:
+        return formats.date_format(log_date, "SHORT_DATE_FORMAT")
+    else:
+        return (formats.date_format(log_date, "SHORT_DATE_FORMAT") + ' ' +
+                formats.date_format(log_date, "TIME_WITH_SECONDS_FORMAT"))
+
+register.filter('log_date_privacy', log_date_privacy)
+
+
+def log_player_privacy(logentry, theshold_days):
+    now = timezone.now()
+    delta = now - logentry.date
+    try:
+        if delta.days >= theshold_days:
+            return logentry.player_name
+        else:
+            return logentry.player.leader
+    except AttributeError:
+        return '-'
+
+register.filter('log_player_privacy', log_player_privacy)
