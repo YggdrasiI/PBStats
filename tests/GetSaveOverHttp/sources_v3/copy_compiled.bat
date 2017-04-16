6d8d66bbc9299858857f4eb4fc85da62761da5f9
Olaf S <civpbem@gmail.com>
2017-04-16 23:05:16 +0200

    Add version 3 of fast save loader. It now supports DirectIP mode. The host (= player which provide the save) had to open Port 2055 (changeable with -P flag) for incomming TCP traffic.

---
+++ tests/GetSaveOverHttp/sources_v3/copy_compiled.bat
@@ -0,0 +1,6 @@
+copy Webserver\Release\Webserver.lib Lib\.
+copy Webserver\Release\Webserver.dll Release\.
+copy CivSaveOverHttp\Release\CivSaveOverHttp.dll Release\.
+copy Injektor\Release\Injektor.exe Release\BTS_Wrapper.exe 
+
+pause
