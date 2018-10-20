50506ebb9ebaa80f4d472a8bdb5b233999e0f28b
Olaf Schulz <funwithkinect@googlemail.com>
2018-10-20 12:48:00 +0200

    Propagate request.user to pb_chat function.

--- civdj/pbspy/views.py
+++ civdj/pbspy/views.py
@@ -423,7 +423,7 @@ def game_manage(request, game_id, action=""):
         elif action == 'chat':
             form = GameManagementChatForm(request.POST)
             if form.is_valid():
-                game.pb_chat(form.cleaned_data['message'])
+                game.pb_chat(form.cleaned_data['message'], request.user)
                 return HttpResponse('chat message sent.', status=200)
             context['chat_form'] = form
         elif action == 'motd':
