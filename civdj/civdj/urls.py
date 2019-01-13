11dce506b4e9f533b8486ce5e4febc50215dd0dc
zulan <git@zulan.net>
2019-01-13 11:24:54 +0100

    fix old include

--- civdj/civdj/urls.py
+++ civdj/civdj/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 urlpatterns = [
     # Examples:
     # url(r'^$', 'civdj.views.home', name='home'),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     # For now we simply redirect anything to pbspy!
     url(r'', include('pbspy.urls')),
 ]
