556665ed6a706a8f2cff8550316240e274cb7e83
Olaf Schulz <funwithkinect@googlemail.com>
2019-04-15 18:26:12 +0200

    PB Mod/pyWB: Made helper script runable on Linux

--- PB Mod_v8/Assets/Python/pyWB/generateWBKeys.py
+++ PB Mod_v8/Assets/Python/pyWB/generateWBKeys.py
@@ -1,13 +1,17 @@
-#!/usr/python
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
 import os
+import os.path
 import re
 
 # XML file with keys for civilization releated keys.
 # Second path is fallback if mod does not contain an own version of the xml file.
-CIV_FILES = [("..\..\..\Assets\XML\Civilizations\CIV4CivilizationInfos.xml", 
-    "..\..\..\..\..\Assets\XML\Civilizations\CIV4CivilizationInfos.xml"), 
-    ]
+CIV_FILES = [(os.path.join("..", "..", "..", "Assets", "XML",
+                           "Civilizations", "CIV4CivilizationInfos.xml"),
+              os.path.join("..", "..", "..", "..", "..", "Assets", "XML",
+                           "Civilizations", "CIV4CivilizationInfos.xml"))
+            ]
 
 CivDescKeys = set()
 CivAdjectiveKeys = set()
@@ -23,13 +27,13 @@ def write_CvWBKeys_auto():
     civ_adj = list(CivAdjectiveKeys)
     leader = list(LeaderKeys)
     city = list(CityKeys)
-    
+
     civ_desc.sort()
     civ_adj.sort()
     leader.sort()
     city.sort()
 
-    text = """
+    text = """\
 CivDescKeys = [
 {civ_desc}
 ]
@@ -43,38 +47,38 @@ UnitNameKeys = [ ]  # not used
 CityKeys = [
 {city}
 ]
-""".format(
+    """.format(
         civ_desc="'" + "',\n'".join(civ_desc) + "'" if len(civ_desc) else "",
         civ_adj="'" + "',\n'".join(civ_adj) + "'" if len(civ_adj) else "",
         leader="'" + "',\n'".join(leader) + "'" if len(leader) else "",
         city="'" + "',\n'".join(city) + "'" if len(city) else "",
-        )
+    )
     with open("CvWBKeys_auto.py", "w") as f:
         f.write(text)
 
 def process_CIV4CivilizationInfos(f):
     bla = [
-            {
-                "reg": re.compile(".*<Description>(.*)</Description>.*"),
-                "arr": CivDescKeys,
-                },
-            {
-                "reg": re.compile(".*<ShortDescription>(.*)</ShortDescription>.*"),
-                "arr": CivDescKeys,
-                },
-            {
-                "reg": re.compile(".*<Adjective>(.*)</Adjective>.*"),
-                "arr": CivAdjectiveKeys,
-                },
-            {
-                "reg": re.compile(".*<LeaderName>(.*)</LeaderName>.*"),
-                "arr": LeaderKeys,
-                },
-            {
-                "reg": re.compile(".*<City>(.*)</City>.*"),
-                "arr": CityKeys,
-                },
-            ]
+        {
+            "reg": re.compile(".*<Description>(.*)</Description>.*"),
+            "arr": CivDescKeys,
+        },
+        {
+            "reg": re.compile(".*<ShortDescription>(.*)</ShortDescription>.*"),
+            "arr": CivDescKeys,
+        },
+        {
+            "reg": re.compile(".*<Adjective>(.*)</Adjective>.*"),
+            "arr": CivAdjectiveKeys,
+        },
+        {
+            "reg": re.compile(".*<LeaderName>(.*)</LeaderName>.*"),
+            "arr": LeaderKeys,
+        },
+        {
+            "reg": re.compile(".*<City>(.*)</City>.*"),
+            "arr": CityKeys,
+        },
+    ]
 
     text = f.readline()
     while text:
@@ -91,15 +95,18 @@ def load_CIV4CivilizationInfos():
         try:
             with open(fname, "r") as f:
                 process_CIV4CivilizationInfos(f)
-                    
+
         except FileNotFoundError:
             continue
 
-        break
+        return
+
+    print("Error: No existing path found for "
+          "'{}'.".format(os.path.basename(fname)))
 
 
 if __name__ == "__main__":
     load_CIV4CivilizationInfos()
     write_CvWBKeys_auto()
-    
+
     #<City>TXT_KEY_CITY_NAME_WASHINGTON</City>
