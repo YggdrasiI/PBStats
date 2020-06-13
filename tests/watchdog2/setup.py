c1f2b847ba637c77902d7f4b72c13bf750cc2fe0
zulan <git@zulan.et>
2020-06-14 00:51:17 +0200

    set patch version

--- tests/watchdog2/setup.py
+++ tests/watchdog2/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="civpb_watchdog",
-    version="2.0",
+    version="2.0.0",
     author="Olaf S.",
     python_requires=">=3.7",
     packages=find_packages(),
